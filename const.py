852ff58210ca02ac8a714e76a7496551787b4f65
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2023-02-25 15:28:15 +0600

    add all fruits

--- const.py
+++ const.py
@@ -29,10 +29,9 @@ REAL_FRUITS_DIR: tp.Final[Path] = DATA_DIR / 'real_fruits'
 
 # Fruits
 FRUITS_NAMES: tp.Tuple[str, ...] = (
-    'Apple', 'Banana', 'Tomatoes', 'Orange',
-
-    # 'Apple', 'Banana', 'Carambola',
-    # 'Guava', 'Kiwi', 'Mango', 'muskmelon',
-    # 'Orange', 'Peach', 'Pear', 'Persimmon',
-    # 'Pitaya', 'Plum', 'Pomegranate', 'Tomatoes'
+    # 'Apple', 'Banana', 'Tomatoes', 'Orange',
+    'Apple', 'Banana', 'Carambola',
+    'Guava', 'Kiwi', 'Mango', 'muskmelon',
+    'Orange', 'Peach', 'Pear', 'Persimmon',
+    'Pitaya', 'Plum', 'Pomegranate', 'Tomatoes'
 )
