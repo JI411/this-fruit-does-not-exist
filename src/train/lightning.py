3b9aeffcb25b27e6b97194d43ebbea183104fd3b
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2023-02-28 04:21:21 +0600

    Fix logging

--- src/train/lightning.py
+++ src/train/lightning.py
@@ -2,7 +2,6 @@
 
 import typing as tp
 
-import cv2
 import numpy as np
 import pytorch_lightning as pl
 import torch
@@ -39,7 +38,7 @@ class BaseFruitSegmentationModule(pl.LightningModule):
         """Train model on batch."""
         predict = self.model(batch=batch['image'])
         score = self.loss.forward(y_pred=predict, y_true=batch['mask'])
-        self.log("train_loss", score)
+        self.log("train_loss", score, on_step=True, logger=True)
 
         if self.logger is not None and self.current_epoch % 10 == 0 and batch_idx % 25 == 0:
             sample, original_sample = batch['image'][0][None], batch['original_image'][0][None]
@@ -52,11 +51,15 @@ class BaseFruitSegmentationModule(pl.LightningModule):
         predict = tensor_to_numpy_image(torch.sigmoid(predict))
         predict -= predict.min()
         predict /= predict.max() + 1e-5
-        predict = cv2.cvtColor(predict, cv2.COLOR_GRAY2RGB)
         original_image = tensor_to_numpy_image(original_sample)
-        self.logger.log_image(key=key, images=[
-            (predict > th) * original_image for th in self.mask_logging_thresholds
-        ])
+        self.logger.log_image(
+            key=f'{key}',
+            images=[original_image for _ in self.mask_logging_thresholds],
+            masks=[
+                {"prediction": {"mask_data": (predict > th), "class_labels": {0: "fruit"}}}
+                for th in self.mask_logging_thresholds
+            ]
+        )
 
     def train_dataloader(self) -> DataLoader:
         """Get train dataloader."""
