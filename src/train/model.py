c5e3dd131b5d230b9f5829c0804328c197cd325f
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2023-03-01 11:36:10 +0600

    Fix images saving

--- src/train/model.py
+++ src/train/model.py
@@ -28,24 +28,3 @@ class UnetWrapper(BaseModel):
 
     def forward(self, batch: torch.Tensor) -> torch.Tensor:
         return self.unet(x=batch)
-
-
-class UnetMetricLearningClassifier(UnetWrapper):
-    """Wrapper for smp.Unet model."""
-    def __init__(self, encoder_name: str = 'resnet34'):
-        """Create Unet model."""
-        super().__init__(encoder_name=encoder_name)
-
-    def forward(self, x):
-        """Sequentially pass `x` trough model`s encoder, decoder and heads"""
-
-        features = self.unet.encoder(x)
-        decoder_output = self.unet.decoder(*features)
-
-        masks = self.segmentation_head(decoder_output)
-
-        if self.classification_head is not None:
-            labels = self.classification_head(features[-1])
-            return masks, labels
-
-        return masks
